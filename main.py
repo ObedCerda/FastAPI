9b1314963553828634839aafda023e9e055fc0d2
Obed Cerda <obed.cerda.dev@gmail.com>
2025-08-20 19:06:48 -0600

    BaseModel example and type hints

--- main.py
+++ main.py
@@ -11,3 +11,18 @@ async def root(): # Cuando se accede a un server se ncecita una funcion asincron
 @app.get("/url")
 async def url():
     return { "url": "https://github.com/obedcerda" }
+
+class User(BaseModel):
+    id: int
+    name : str  = "Obed Cerda"
+    signup_ts: datetime | None = None #  can be date | or none
+    friends: list[int] = []
+
+external_data= {
+    "id": 1234,
+    "signup_ts": "2017-06-01 12:22",
+    "friends": [1, "2", b"3"],
+}
+
+user = User(**external_data) # Pydantic valida los datos y crea un objeto User
+print(user) # Imprime el objeto User creado
